ed90fd96419b684f7d3fbd42231e1345ec749142
git-leon <xleonhunter@gmail.com>
2023-02-22 17:45:12 -0500

    update

--- src/main/java/com/github/curriculeon/controller/GptController.java
+++ src/main/java/com/github/curriculeon/controller/GptController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
